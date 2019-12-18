481fae9e5ad6dee83172d1214f90cc1bf9774b60
Chris K <chriskwok@local>
2019-12-17 20:36:53 -0800

    add data transform object for mixtape
    - add unit test for MixtapeDTO

---
+++ src/main/java/com/highspot/assignment/dto/mixtape/SongDTO.java
@@ -0,0 +1,32 @@
+package com.highspot.assignment.dto.mixtape;
+
+public class SongDTO {
+
+    private Long id;
+    private String artist;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
